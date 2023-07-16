245ecd7aa6ff67687ba86afbaedf5ba0320cb306
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-07-16 23:56:05 +1000

    disabled styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,18 +6,28 @@ interface Props {
     onClick?: () => void;
     variant?: 'primary' | 'danger';
     grow?: boolean;
+    type?: 'button' | 'submit' | 'reset';
 }
 
-const Button = ({ children, disabled, onClick, variant='primary', grow=false }:Props) => 
+const Button = ({ children, disabled, onClick, variant='primary', grow=false, type="button" }:Props) => 
     <button 
         disabled={disabled} 
         onClick={onClick} 
+        type={type}
         className={`
             flex justify-center items-center gap-3
             rounded-[10px] px-[21px] py-3
             outline outline-1
+            transition-all duration-200
             ${grow ? 'w-full' : 'w-fit'}
-            ${variant == 'primary' ? 'bg-avertroBlue text-white' : 'bg-transparent text-danger outline-danger'}
+            ${
+                variant === 'primary' 
+                    ? 'bg-avertroBlue text-white outline-avertroBlue hover:bg-avertroBlue/80' 
+                    : 'bg-transparent text-danger outline-danger hover:bg-danger/10'
+            }
+            ${
+                disabled ? 'cursor-not-allowed bg-neutral-500 outline-neutral-500 hover:bg-neutral-500/80' : 'cursor-pointer'
+            }
         `} 
         >
         
