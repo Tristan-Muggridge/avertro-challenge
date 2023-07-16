669d0963d4c06ff61f3100fdd8b4fb6765391bcd
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-07-16 20:10:07 +1000

    Implement new components

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,31 @@ import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Tabs from './components/Tabs';
 import Divider from './components/Divider';
+import { Tab } from './types';
+import Objectives from './components/Objectives';
 
-const tabs = [
+const tabs: Tab[] = [
 	{
 		heading: "Mission & Vision",
-		content: <>m&v</>
+		content: <div className='text-center font-bold'> To be implemented! </div>
 	},
 	{
 		heading: "Strategic Business Objectives",
-		content: <>sbo</>
+		content: 
+		<>
+			<Objectives />
+		</>
 	},
-
 ]
 
-export default () => {
+export default () => {	
 	return (
 		<>
 			<Navigation />
 			
 			<main className="bg-neutral-100 min-h-[calc(100vh-72px)]">	
 			
-				<article className='mx-[38px] flex flex-col gap-6 py-6  xl:max-w-screen-xl xl:mx-auto'>
+				<article className='mx-[38px] flex flex-col gap-6 py-6 xl:max-w-screen-xl xl:mx-auto'>
 
 					<Header content='Set Security Strategy'/>
 					<Divider />
