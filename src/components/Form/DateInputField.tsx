87af84d2473d3ee8eb93ab33f662469913730afd
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-07-17 00:26:36 +1000

    make both fields more consistent
    todo: extract commonality elsewhere

--- src/components/Form/DateInputField.tsx
+++ src/components/Form/DateInputField.tsx
@@ -1,17 +1,23 @@
-import { ChangeEvent, createRef, useState } from "react";
+import { createRef } from "react";
 import { AiFillCalendar } from "react-icons/ai";
 import { InputFieldProps } from "./InputField";
 
 interface DateInputProps {
     min: string;
 }
-const DateInputField = ({label, value, onChange, name, min, require}: InputFieldProps & DateInputProps) => {
+
+const DateInputField = ({label, value, onChange, name, min, require, children}: InputFieldProps & DateInputProps) => {
     
     const dateRef = createRef<HTMLInputElement>();
 
     return (
         <div className='flex flex-col flex-1'>
-            <label htmlFor="name" className='text-avertroBlue font-inter font-bold text-xl'>{label}</label>
+            
+            <div className='flex justify-between flex-wrap'>
+                {label && <label htmlFor="name" className='text-avertroBlue font-inter font-bold text-xl'>{label}</label>}
+                {children}
+            </div>
+
             <div onClick={() => dateRef.current?.focus()} 
                  {...{value, onChange, name, min}} 
                  className='relative outline outline-2 outline-grey rounded-md py-4 px-2 mt-2 flex gap-2 items-center justify-center' >
--- src/components/Form/TextInputField.tsx
+++ src/components/Form/TextInputField.tsx
@@ -2,11 +2,13 @@ import { InputFieldProps } from "./InputField";
 
 const TextInputField = ({label, value, onChange, name, children, require}:InputFieldProps) => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col flex-1'>
+            
             <div className='flex justify-between flex-wrap'>
                 {label && <label htmlFor="name" className='text-avertroBlue font-inter font-bold text-xl'>{label}</label>}
                 {children}
             </div>
+            
             <input type="text" {...{value, onChange, name, require}} className='outline outline-2 outline-grey rounded-md p-4 mt-2' />
         </div>
     )
