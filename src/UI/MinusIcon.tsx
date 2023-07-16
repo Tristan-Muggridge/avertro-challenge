145207be3907678b7b2249355525dcccd5655031
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-07-16 20:06:24 +1000

    Implement + and - icons

---
+++ src/UI/MinusIcon.tsx
@@ -0,0 +1,7 @@
+import { BiMinus } from "react-icons/bi";
+
+const MinusIcon = ({className}:{className?:string}) => 
+    <BiMinus className={`rounded-full w-5 h-5 ${className}`}/>
+
+
+export default MinusIcon;
---
+++ src/UI/PlusIcon.tsx
@@ -0,0 +1,7 @@
+import { BiPlus } from "react-icons/bi";
+
+const PlusIcon = ({className}:{className?:string}) => 
+    <BiPlus className={`rounded-full w-5 h-5 ${className}`}/>
+
+
+export default PlusIcon;
