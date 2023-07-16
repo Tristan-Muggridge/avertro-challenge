7816eac5f159d5051c004f4114b0bfd664f931f1
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-07-16 20:06:13 +1000

    Implement LocalStorage
    Makes LocalStorage a bit more useful providing ability to use type assertions in the getter

---
+++ src/LocalStorage.ts
@@ -0,0 +1,35 @@
+
+import { ILocalStorage } from "./types";
+
+class LocalStorage implements ILocalStorage {
+    get<T>(key:string): T | null {
+                
+        const value = localStorage.getItem(key);
+        return value
+            ? JSON.parse(value) as T
+            : null;
+    }
+
+    set<T>(key:string, value: T): void {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    delete(key:string):void {
+        localStorage.removeItem(key);
+    }
+
+    clear():void {
+        localStorage.clear();
+    }
+
+    private static instance: LocalStorage;
+    private constructor() {}
+    public static getInstance(): LocalStorage {
+        if (!LocalStorage.instance) {
+            LocalStorage.instance = new LocalStorage();
+        }
+        return LocalStorage.instance;
+    }
+}
+
+export default LocalStorage;
--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,26 @@ export interface Tab {
 export interface TabGroup {
     tabs: Tab[];
 }
+
+export interface KeyMeasure {
+    id: number;
+    name: string;
+}
+
+export interface Objective {
+    id: number;
+    name: string;
+    keyMeasures: KeyMeasure[];
+    startDate: Date;
+    endDate: Date;
+
+    createdDate: Date;
+    updatedDate: Date;
+}
+
+export interface ILocalStorage {
+    get<T>(key:string): T | null;
+    set<T>(key:string, value: T): void;
+    delete(key:string): void;
+    clear(): void;
+}
